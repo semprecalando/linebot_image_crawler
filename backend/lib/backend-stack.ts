cbe641e05e2e84b5e58deb06e735229b1c237113
semprecalando <dansefog@yahoo.co.jp>
2022-08-07 13:05:52 +0000

    set S3 default image

--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -5,11 +5,13 @@ import { Construct } from 'constructs';
 import { createImageCrawlerLambda, createFaceMatcherLambda } from './lambda/lambda-stack';
 import { createWebhookAPI } from './api-gw/line-webhook-api';
 import { createImageBucket, createFaceBucket } from './s3/bucket-stack';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
 
 export class LineBotImageCrawlerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
+    // 各種コンポーネントを定義
     const imageCrowlerRole = createImageCrowlerRole(this);
     const faceMacherRole = createFaceMatcherRole(this);
 
@@ -22,5 +24,11 @@ export class LineBotImageCrawlerStack extends Stack {
     const faceDetectTable = createFaceDetectTable(this);
     const faceMatcherLambda = createFaceMatcherLambda(this, imageBucket, faceSourceBucket, faceDetectTable, faceMacherRole);
     faceDetectTable.grantReadWriteData(faceMatcherLambda);
+
+    // S3に投入するファイルを設定(ディレクトリごと投入する)
+    const faceImageDeployment = new BucketDeployment(this, 'deployFaceImages', {
+      sources: [Source.asset('./faceImages')],
+      destinationBucket: faceSourceBucket
+    });
   }
 }
