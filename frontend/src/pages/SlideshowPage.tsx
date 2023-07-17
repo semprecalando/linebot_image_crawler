88cc7fc272b536f26719677e5cb40cb77eedbc16
semprecalando <dansefog@yahoo.co.jp>
2023-07-17 04:16:19 +0000

    impl refresh logic

--- frontend/src/pages/SlideshowPage.tsx
+++ frontend/src/pages/SlideshowPage.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect, useState } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { CLOUDFRONT_URL, WS_URL } from '../lib/settings';
 import { getImageThumbnailList } from '../lib/dynamoAPI';
 import { CorkboardImage } from  '../components/CorkboardImage';
@@ -9,7 +9,9 @@ import { ImageConfig } from '../lib/types';
 import { Queue, createRandomArrayN, getCellPosition, getRandomPosition, useTimer } from '../lib/utils';
 
 const MAX_IMAGE_NUM = 8;
+// Todo: PX_SIZEを可変にする
 const IMAGE_PX_SIZE = 350;
+const MAX_REFRESH_COUNT = 24;
 
 interface SlideshowImageConfig extends ImageConfig {
   imageName: string
@@ -33,25 +35,29 @@ const SlideshowPage: FC = () => {
   const [thumbnailList, setThumbnailList] = useState<string[]>([]);
   const [thumbnailWsQueue, setThumbnailWsQueue] = useState<Queue<string>>(new Queue<string>());
   const [slideshowImageConfigList, setSlideshowImageConfigList] = useState<SlideshowImageConfig[]>([]);
-  const [count, setCount] = useState<number>(0);
+  const [oldestImageId, setOldestImageId] = useState<number>(0);
+  const notRefreshCountRef = useRef<number>(0);
   const [firstFlag, setFirstFlag] = useState<boolean>(true);
   const [width, height] = useWindowSize();
 
   const onTimerEnd = () => {
-    if (thumbnailWsQueue.size() > 0) {
-      // キューが空でなければ1つデキューし、新しいthumbnailListを作る
-      // const newImage = newThumbnailWsQueue.dequeue();
-      const newImage = thumbnailWsQueue.dequeue();
-      const newThumbnailList = thumbnailList.concat();
-      if (newImage !== undefined && !thumbnailList.includes(newImage)) {
-        newThumbnailList.push(newImage);
-        setTimeout(() => {
+    setTimeout(() => {
+      if (thumbnailWsQueue.size() > 0) {
+        // キューが空でなければ1つデキューし、新しいthumbnailListを作る
+        const newImage = thumbnailWsQueue.dequeue();
+        const newThumbnailList = thumbnailList.concat();
+        if (newImage !== undefined && !thumbnailList.includes(newImage)) {
+          newThumbnailList.push(newImage);
           setThumbnailList(newThumbnailList);
-        });
-        // setThumbnailWsQueue(newThumbnailWsQueue);
-        // Todo: 一定数以上同時に送られるとデキューできない
+          // 何連続キューが空だったかのカウンタをリセット
+          notRefreshCountRef.current = 0;
+        }
+      } else {
+        const newCount = notRefreshCountRef.current + 1;
+        console.log("count up" + newCount);
+        notRefreshCountRef.current = newCount;
       }
-    }
+    });
   }
 
   const { second, startTimer, stopTimer, resetAndStart } = useTimer(5, onTimerEnd);
@@ -79,11 +85,6 @@ const SlideshowPage: FC = () => {
       const messageData = JSON.parse(event.data);
       // ダブっていなければ追加（重複受信対策）
       if (messageData.imageName && !thumbnailList.includes(messageData.imageName) && !thumbnailWsQueue.toArray().includes(messageData.imageName)) {
-        //newThumbnailList.push(messageData.imageName);
-        //setThumbnailList(newThumbnailList);
-        // wsキューに画像追加
-        //newThumbnailWsQueue.enqueue(messageData.imageName);
-        //setThumbnailWsQueue(newThumbnailWsQueue);
         thumbnailWsQueue.enqueue(messageData.imageName);
       }
     }
@@ -100,51 +101,64 @@ const SlideshowPage: FC = () => {
     }
   },[]);
 
-  // Todo: なぜか同じ画像が0番に来る問題の解決（おそらくuseEffectが2回呼ばれている）
+  const refreshImage = () => {
+    console.log("all refresh");
+    setFirstFlag(false);
+    const newSlideshowImageConfigList: SlideshowImageConfig[] = [];
+    // 表示数以上の画像データがとれた場合、画像をランダムに選択
+    const pickedList: string[] = thumbnailList.length <= MAX_IMAGE_NUM ? thumbnailList : createRandomArrayN(thumbnailList, MAX_IMAGE_NUM);
+    pickedList.map((thumbnail, index) => {
+      const rotationAbsoluteDeg = 10;
+      const rotationDeg = Math.random() * (rotationAbsoluteDeg - (-rotationAbsoluteDeg)) - rotationAbsoluteDeg;
+      const imagePosition = getCellPosition(width, height, index, MAX_IMAGE_NUM);
+      newSlideshowImageConfigList.push({
+        imageName: thumbnail,
+        imagePosition: imagePosition,
+        rotation: rotationDeg
+      });
+      return null;
+    });
+    if (thumbnailList.length <= MAX_IMAGE_NUM) {
+      const nextDaleteImageId = newSlideshowImageConfigList.length;
+      setOldestImageId(nextDaleteImageId >= MAX_IMAGE_NUM - 1 ? 0 : nextDaleteImageId);
+    }
+    setSlideshowImageConfigList(newSlideshowImageConfigList);
+    notRefreshCountRef.current = 0;
+  }
+
   // ランダムに画像を総入れ替え
   useEffect(() => {
     if(firstFlag && thumbnailList.length > 0) {
-      console.log("first");
-      setFirstFlag(false);
-      const newSlideshowImageConfigList: SlideshowImageConfig[] = [];
-      // 表示数以上の画像データがとれた場合、画像をランダムに選択
-      const pickedList: string[] = thumbnailList.length <= MAX_IMAGE_NUM ? thumbnailList : createRandomArrayN(thumbnailList, MAX_IMAGE_NUM);
-      pickedList.map((thumbnail, index) => {
-        const rotationAbsoluteDeg = 10;
-        const rotationDeg = Math.random() * (rotationAbsoluteDeg - (-rotationAbsoluteDeg)) - rotationAbsoluteDeg;
-        const imagePosition = getCellPosition(width, height, index, MAX_IMAGE_NUM);
-        newSlideshowImageConfigList.push({
-          imageName: thumbnail,
-          imagePosition: imagePosition,
-          rotation: rotationDeg
-        });
-        return null;
-      });
-      if (thumbnailList.length <= MAX_IMAGE_NUM) {
-        const newCount = newSlideshowImageConfigList.length;
-        setCount(newCount >= MAX_IMAGE_NUM - 1 ? 0 : newCount);
-      }
-      setSlideshowImageConfigList(newSlideshowImageConfigList);
+      refreshImage();
     }
     else if (thumbnailList.length > 0) {
-      console.log("second");
       const newSlideshowImageConfigList = [...slideshowImageConfigList];
       const latestThumbnail = thumbnailList.at(-1);
-      if (latestThumbnail) {
+      if (latestThumbnail && latestThumbnail) {
+        console.log("change image");
         const rotationAbsoluteDeg = 10;
         const rotationDeg = Math.random() * (rotationAbsoluteDeg - (-rotationAbsoluteDeg)) - rotationAbsoluteDeg;
-        const imagePosition = getCellPosition(width, height, count, MAX_IMAGE_NUM);
-        newSlideshowImageConfigList[count] = {
+        const imagePosition = getCellPosition(width, height, oldestImageId, MAX_IMAGE_NUM);
+        newSlideshowImageConfigList[oldestImageId] = {
           imageName: latestThumbnail,
           imagePosition: imagePosition,
           rotation: rotationDeg
         };
         setSlideshowImageConfigList(newSlideshowImageConfigList);
-        const newCount = count >= (MAX_IMAGE_NUM - 1) ? 0 : count + 1;
-        setCount(newCount);
+        const nextDaleteImageId = oldestImageId >= (MAX_IMAGE_NUM - 1) ? 0 : oldestImageId + 1;
+        setOldestImageId(nextDaleteImageId);
       }
     }
   },[thumbnailList]);
+
+  useEffect(() => {
+    // Todo: refreshImageをした際に、一度でもキューからデータ更新していると画像が一枚だけになる
+    if (notRefreshCountRef.current >= MAX_REFRESH_COUNT) {
+      setThumbnailWsQueue(new Queue<string>());
+      refreshImage();
+      notRefreshCountRef.current = 0;
+    }
+  }, [notRefreshCountRef.current]);
 
   return (
     <div>
